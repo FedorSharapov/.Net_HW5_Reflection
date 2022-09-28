2f8f58242984c3db60ab9078894fcc442fcc6550
Федор Шарапов <sharapovfedor@yandex.ru>
2022-09-28 19:46:37 +0300

    Тестовый класс

---
+++ src/Reflection/F.cs
@@ -0,0 +1,11 @@
+﻿namespace Reflection
+{
+    internal class F
+    {
+        public int I1 { get; set; } = 1;
+        public int I2 { get; set; } = 2;
+        public int I3 { get; set; } = 3;
+        public int I4 { get; set; } = 4;
+        public int I5 { get; set; } = 5;
+    }
+}
