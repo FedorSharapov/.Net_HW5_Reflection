6365dd3380b7b2fbbbbebb81642de962e113017f
Федор Шарапов <sharapovfedor@yandex.ru>
2022-09-28 19:51:40 +0300

    Класс помощник вывода текста в консоль

---
+++ src/Reflection/ConsoleHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace Reflection
+{
+    static class ConsoleHelper
+    {
+        // usage: WriteLine("This is my [message] with inline [color] changes.", ConsoleColor.Yellow);
+        public static void WriteLine(string message, ConsoleColor color)
+        {
+            var pieces = Regex.Split(message, @"(\[[^\]]*\])");
+
+            foreach (var piece in pieces)
+            {
+                if (piece.StartsWith("[") && piece.EndsWith("]"))
+                {
+                    Console.ForegroundColor = color;
+                    Console.Write(piece.ToArray(), 1, piece.Length - 2);
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                    Console.Write(piece);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
