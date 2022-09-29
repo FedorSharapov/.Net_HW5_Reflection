9fab0f2b709ae61978115bdc7200eec1a804437a
Федор Шарапов <sharapovfedor@yandex.ru>
2022-09-29 18:01:59 +0300

    описание методов

--- src/Reflection/Serializers/ISerializer.cs
+++ src/Reflection/Serializers/ISerializer.cs
@@ -2,6 +2,12 @@
 {
     internal interface ISerializer
     {
+        /// <summary>
+        /// сериализовать 
+        /// </summary>
+        /// <typeparam name="T">тип сериализуемого объекта</typeparam>
+        /// <param name="value">сериализуемый объект</param>
+        /// <returns>сериализованный объект в строку</returns>
         public string Serialize<T>(T value);
     }
 }
--- src/Reflection/Serializers/MyJson.cs
+++ src/Reflection/Serializers/MyJson.cs
@@ -7,6 +7,13 @@ namespace Reflection.Serializers
     {
         static private StringBuilder _result = new StringBuilder(64);
 
+        /// <summary>
+        /// Сериализация свойств объекта в Json формат
+        /// </summary>
+        /// <typeparam name="T">тип объекта сериализации</typeparam>
+        /// <param name="value">объект сериализации</param>
+        /// <returns>сериализованный объект в Json формате</returns>
+        /// <exception cref="ArgumentNullException">объект сериализации = null</exception>
         public static string Serialize<T>(T value)
         {
             if (value == null)
@@ -22,7 +29,7 @@ namespace Reflection.Serializers
                 _result.Append("\"");
                 _result.Append(properties[i].Name);
                 _result.Append("\":");
-                GetJsonValue(properties[i].GetValue(value), properties[i].PropertyType);
+                AddJsonValue(properties[i].GetValue(value), properties[i].PropertyType);
 
                 if (i != lastI)
                     _result.Append(",");
@@ -32,14 +39,20 @@ namespace Reflection.Serializers
             return _result.ToString();
         }
 
-        static void GetJsonValue(object obj, Type type)
+        /// <summary>
+        /// Добавление значения в сериализуемый объект
+        /// </summary>
+        /// <param name="value">значение</param>
+        /// <param name="type">тип значения</param>
+        /// <exception cref="Exception">Неизвестный тип значения</exception>
+        static void AddJsonValue(object value, Type type)
         {
             if (type == typeof(int))
-                _result.Append(obj.ToString());
+                _result.Append(value.ToString());
             else if (type == typeof(string))
             {
                 _result.Append("\"");
-                _result.Append(obj);
+                _result.Append(value);
                 _result.Append("\"");
             }
             else
