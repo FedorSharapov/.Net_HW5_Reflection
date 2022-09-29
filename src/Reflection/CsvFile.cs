95c35a2e78a828d6de87665257ff6d19912d3dbb
Федор Шарапов <sharapovfedor@yandex.ru>
2022-09-29 17:59:44 +0300

    csv файл (запись и чтение)

---
+++ src/Reflection/CsvFile.cs
@@ -0,0 +1,29 @@
+﻿namespace Reflection
+{
+    internal static class CsvFile
+    {
+        /// <summary>
+        /// Запись в файл
+        /// </summary>
+        /// <param name="path">путь к файлу</param>
+        /// <param name="data">данные</param>
+        public static void Write(string path, string data)
+        {
+            using var streamWriter = new StreamWriter(path);
+            streamWriter.Write(data);
+
+            streamWriter.Close();
+        }
+
+        /// <summary>
+        /// Чтения из файла
+        /// </summary>
+        /// <param name="path">путь к файлу</param>
+        /// <returns>данные файла</returns>
+        public static string Read(string path)
+        {
+            using var streamReader = new StreamReader(path);
+            return streamReader.ReadToEnd();
+        }
+    }
+}
